e3e9020253ccb9cd9443b683185d17ce8f90fd92
sg1110 <saule.guzyte@tesco.com>
2019-01-24 10:17:06 +0000

    If the birthday is today page is redirected to wish HP, needs tests

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'date'
 
 
 class Calendar < Sinatra::Base
@@ -11,14 +12,19 @@ class Calendar < Sinatra::Base
   post '/birthday' do
     p params
     session[:entered_name] = params[:entered_name]
-    redirect '/your_birthday'
+    session[:entered_date] = params[:entered_date]
+
+    @date=session[:entered_date].to_s
+    @todays_date = Date.today.to_s
+    redirect '/your_birthday' if @date == @todays_date
   end
 
   get '/your_birthday' do
     @entered_name=session[:entered_name]
+    @date=session[:entered_date]
     erb :birthday
   end
 
   # start the server if ruby file executed directly
-  run! if app_file ==$0
+   run! if app_file ==$0
 end
