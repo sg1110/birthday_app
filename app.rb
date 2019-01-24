f0418c1c87733e442340b3989d9b5a60115c826c
sg1110 <saule.guzyte@tesco.com>
2019-01-24 11:48:53 +0000

    tells age in dates and days left until the birthday

--- app.rb
+++ app.rb
@@ -14,16 +14,39 @@ class Calendar < Sinatra::Base
     session[:entered_name] = params[:entered_name]
     session[:entered_date] = params[:entered_date]
 
-    @date=session[:entered_date].to_s
-    @todays_date = Date.today.to_s #why it needs to be changed
-    #string??? p of Date.today output is 2019-01-24
+    @date=session[:entered_date]
+    @todays_date = Date.today.to_s
     redirect '/your_birthday' if @date == @todays_date
+    redirect '/not_your_birthday' if @date != @todays_date
+
   end
 
   get '/your_birthday' do
     @entered_name=session[:entered_name]
     @date=session[:entered_date]
     erb :birthday
+  end
+
+  get '/not_your_birthday' do
+    @entered_name=session[:entered_name]
+    @date=session[:entered_date]
+
+    #calculates age in days
+    @date=Date.parse(@date)
+    @todays_date = Date.today
+    @age=(@todays_date-@date).to_i
+
+    next_year = Date.today.year + 1
+    next_bday = "#{@date.day}-#{@date.month}-#{next_year}"
+
+    @days_left = ((Date.parse(next_bday) - Date.today).to_i)-365
+      if @days_left > 0   # if the date has not passed this year
+        @days_left
+      elsif @days_left < 0 #if the date has already passed this year
+        @days_left += 365
+      end
+
+    erb :not_birthday
   end
 
   # start the server if ruby file executed directly
--- spec/features/entered_data_spec.rb
+++ spec/features/entered_data_spec.rb
@@ -1,10 +1,18 @@
+require 'date'
+
 feature 'Enter your name' do
   scenario 'When todays date is entered it redirects to congrats page' do
     visit('/')
-    fill_in :entered_name, with: 'Saule'
-    fill_in :entered_date, with: '2019-01-24'
+    fill_in :entered_name, with: 'Charlie'
+    fill_in :entered_date, with: Date.today.to_s
     click_button 'Go!'
     save_and_open_page
-    expect(page).to have_content 'Happy Birthday Saule!'
+    expect(page).to have_content 'Happy Birthday Charlie!'
   end
 end
+#tests missing for:
+#no input
+#future, day
+#date of birth - years....
+#current entry tells how old they will be, not how many days are left
+#extract calculations into lib rb file
